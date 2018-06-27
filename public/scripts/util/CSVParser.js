a89a40dd82994efae60bd1413b5a734da4409638
caicsi <cai.blanchard@mymail.champlain.edu>
2018-06-27 13:33:43 -0400

    CSV parser for overview files

--- public/scripts/util/CSVParser.js
+++ public/scripts/util/CSVParser.js
@@ -39,13 +39,30 @@ function CSVToJSONConverter(Blob, callback) {
 
         let headers = lines.filter(line => line.length > 1)[0];
 
-        //loop through entries (proceeding the headers)
-        //help from http://techslides.com/convert-csv-to-json-in-javascript
-        lines.filter(line => line.length > 1)
-            .slice(1)
-            .forEach(line => {
-            let entry = {};
+        //check if the CSV is the overview; headers will be different
+        if (headers === lines[0]) {
+            json = OverviewCSVtoJSON(lines, json);
+        }
+        //if it's not the overview, parse normally
+        else {
+           json = CSVtoJSON(lines, json, headers);
+        }
 
+        //json.shift(); headers are already removed above
+
+        callback(json, lines.filter(line => line.length === 1 && line[0] !== ""));
+    };
+
+    //set the file reader to read the contents as text
+    reader.readAsText(Blob);
+}
+
+function CSVtoJSON(lines, json, headers) {
+    //loop through entries (proceeding the headers)
+    lines.filter(line => line.length > 1)
+        .slice(1) //remove the headers
+        .forEach(line => {
+            let entry = {};
 
             //loop through columns
             for(let j = 0; j < headers.length; j++) {
@@ -54,14 +71,65 @@ function CSVToJSONConverter(Blob, callback) {
 
             json.push(entry);
         });
+    return json;
+}
 
-        json.shift();
+function OverviewCSVtoJSON(lines, json) {
 
-        callback(json, lines.filter(line => line.length === 1 && line[0] !== ""));
-    };
+    //loop through entries
+    for (let i = 0; i < lines.length; i++) {
+        let entry = {};
 
-    //set the file reader to read the contents as text
-    reader.readAsText(Blob);
+        //remove colons
+        for(let k = 0; k < lines[i].length; k++) {
+            if (lines[i][k][lines[i][k].length - 1] === ":") {
+                lines[i][k] = lines[i][k].substring(0, lines[i][k].length - 1);
+            }
+        }
+
+        //check if line is just a header
+        if (lines[i].length === 1 && lines[i][0].length > 2) {
+            let subJson = {};
+            for (let j = i + 1; j < lines.length; j++) {
+                if (lines[j].length === 1){
+                    break;
+                }
+                subJson[j - (i + 1)] = lines[j];
+                lines[j] = "";
+            }
+            entry[lines[i]] = subJson;
+
+            json.push(entry); //move to below if/else if we want to see the gaps in the CSV file
+        }
+        //otherwise, proceed to make json entry as normal
+        else if (lines[i].length > 1) {
+            entry[lines[i][0]] = lines[i][1];
+
+            json.push(entry); //move to below if/else if we want to see the gaps in the CSV file
+        }
+    }
+
+    //console.log(json);
+
+    return json;
+}
+
+//mini-function to be called in OverviewCSVtoJSON, separated so it can be recursive if need be.
+function createOverviewEntry(line) {
+
+    let entry = {};
+
+    //check if line is just a header
+    if (line.length === 1) {
+        let subJson = createOverviewEntry(line);
+        entry[line[0]] = subJson;
+    }
+    //otherwise, proceed to make json entry
+    else if (line.length > 1) {
+        entry[line[0]] = entry[line[1]];
+    }
+
+    return entry;
 
 }
 
