46d750abb5e66b5af58bb2c2c5d238bfbd657b4a
caicsi <cai.blanchard@mymail.champlain.edu>
2018-07-26 12:58:44 -0400

    Pardot progress

--- public/scripts/util/vueScripts.js
+++ public/scripts/util/vueScripts.js
@@ -25,6 +25,7 @@ let dropBox = new Vue({
         currentBox: 'initial-drop-box',
         styling: {
             "padding": '30%',
+            "vertical-align": "middle",
             "background": 'radial-gradient(#0e76a3, #0d56a2, #0d2b56)'
         }
     }
@@ -174,25 +175,46 @@ checkForReport();
 function XMLtoJSON(data) {
 
     let json = {};
+    let subJson = {};
+    let i = 0;
     data = '<rsp stat="ok" version="1.0"> <stats> <sent>1003</sent> <delivered>543</delivered> <total_clicks>54</total_clicks> <unique_clicks>5</unique_clicks> <soft_bounced>3</soft_bounced> <hard_bounced>2</hard_bounced> <opt_outs>454</opt_outs> <spam_complaints>123</spam_complaints> <opens>34</opens> <unique_opens>...</unique_opens> <delivery_rate>...</delivery_rate> <opens_rate>100%</opens_rate> <click_through_rate>20%</click_through_rate> <unique_click_through_rate>...</unique_click_through_rate> <click_open_ratio>...</click_open_ratio> <opt_out_rate>34%</opt_out_rate> <spam_complaint_rate>0.0</spam_complaint_rate> </stats> </rsp>';
 
-    let parser = new DOMParser();
-    let xmlDoc = parser.parseFromString(data, "text/xml");
-    xmlDoc.documentElement.childNodes.forEach(node => {
-        // if (node.childNodes.length > 1) {
-        //     node.childNodes.forEach(childNode => {
-        //
-        //     })
-        // }
-        // else {
-        json[node.nodeName] = node.nodeValue;
-        //}
+    // let parser = new DOMParser();
+    // let xmlDoc = parser.parseFromString(data, "text/xml");
+    // xmlDoc.documentElement.childNodes.forEach(node => {
+    //     if (node.childNodes.length > 0) {
+    //         subJson = {};
+    //         for (i = 0; i < node.childNodes.length; i++) {
+    //             subJson[node.childNodes[i].nodeName] = node.childNodes.nodeValue;
+    //         }
+    //         // node.childNodes.forEach(childNode => {
+    //         //     subJson[childNode.nodeName] = childNode.nodeValue;
+    //         // });
+    //         json[node.nodeName] = subJson;
+    //     }
+    //     else {
+    //         json[node.nodeName] = node.nodeValue;
+    //     }
+    // });
+    //
+    // console.log(xmlDoc);
+    // console.log("xml parsed:");
+    // console.log(json);
+
+    let json2 = {};
+
+    let stats = xmlDoc.getElementsByTagName("stats");
+    console.log(stats);
+    console.log(stats[0]);
+    console.log(stats[0].childElementCount);
+    stats[0].childNodes.forEach(node => {
+        json2[node.nodeName] = node.textContent;
     });
 
-    console.log("xml parsed:");
-    console.log(json);
+    console.log("xml take two:");
+    console.log(json2);
 
-    return json;
+    //return json;
 }
 
 XMLtoJSON("hello");
