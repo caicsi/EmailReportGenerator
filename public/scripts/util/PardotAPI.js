6adc3c0fdbe027b1553beba68ea8302bb9e99c11
codeWonderland <easter@champlain.edu>
2018-08-01 13:31:53 -0400

    moved pardot auth into function

--- public/scripts/util/PardotAPI.js
+++ public/scripts/util/PardotAPI.js
@@ -1,11 +1,13 @@
-
-
 function getURL(emailId) {
 
-    let pardotAuth = require('../.pardot_auth');
-    return 'https://pi.pardot.com/api/email/version/4/do/stats/id/'
-        + emailId
-        + "?user_key=" + pardotAuth.user_key
-        + "&api_key=" + pardotAuth.api_key
-        + "&list_email_id=" + emailId;
+    const pardotAuth = {
+        user_key : "c885ea7f299411ac8d667fecbecb54ae",
+        api_key : "d925155ecd258eb7dc131728fe4848dc"
+    };
+
+    return 'https://pi.pardot.com/api/email/version/4/do/stats/id/' + 
+    emailId + 
+    "?user_key=" + pardotAuth.user_key + 
+    "&api_key=" + pardotAuth.api_key + 
+    "&list_email_id=" + emailId;
 }
